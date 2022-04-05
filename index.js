1ac7b329f4c26df43cfb36eaa73502b3352cc4e5
longplu <longplu1@gmail.com>
2022-04-05 16:36:46 -0400

    wip

--- index.js
+++ index.js
@@ -33,6 +33,10 @@ io.on('connection', (socket) => {
     });
 });
 
+app.get("/", (req, res) => {
+    res.send("hello world");
+  });
+
 server.listen(process.env.PORT || 3001, () => {
     console.log("SERVER RUNNING")
 });
