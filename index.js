df68fd6018134d6278df2a5553ea09e5b8b4d00c
longplu <longplu1@gmail.com>
2022-04-05 14:24:22 -0400

    wip

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
         // origin: 'https://socket-io-chat-long.netlify.app/',
-        origin: ['*'],
+        origins: ['*'],
 
     handlePreflightRequest: (req, res) => {
         res.writeHead(200, {
