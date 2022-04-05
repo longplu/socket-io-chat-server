2a995f1c597c1f8128a5be5c864fdb6c693f8f8c
longplu <longplu1@gmail.com>
2022-04-05 12:09:29 -0400

    wip: updates connections

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:3000',
+        origin: 'https://socket-io-chat-long.netlify.app',
         methods: ['GET', 'POST'],
     },
 });
@@ -25,7 +25,7 @@ io.on('connection', (socket) => {
     socket.on('send_message', (data) => {
         socket.to(data.room).emit('receive_message', data);
     });
-    
+
     socket.on('disconnect', ()=> {
         console.log(`User Disconnected: ${socket.id}`);
     });
